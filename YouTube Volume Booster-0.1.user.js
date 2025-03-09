09d2a383ca4ffe6d5515be9b9fcb9729163b325b
AsamKeping <h34rt54@gmail.com>
2025-03-10 01:30:27 +0800

    Update YouTube Volume Booster-0.1.user.js
    Fix : Volume bar not appearing when page refreshed.

--- YouTube Volume Booster-0.1.user.js
+++ YouTube Volume Booster-0.1.user.js
@@ -5,6 +5,7 @@
 // @description  Boost YouTube volume up to 500% by default, with a toggle button to increase limit to 1000%.
 // @author       obiyomida
 // @match        *://www.youtube.com/watch*
+// @match        *://www.youtube.com/*
 // @grant        none
 // ==/UserScript==
 
