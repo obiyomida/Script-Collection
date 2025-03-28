871c5115117bb49eef4712a7ff313d95356243fb
AsamKeping <h34rt54@gmail.com>
2025-03-29 02:51:18 +0800

    Update Facebook Ad & Sidebar Blocker-1.3.user.js

--- Facebook Ad & Sidebar Blocker-1.3.user.js
+++ Facebook Ad & Sidebar Blocker-1.3.user.js
@@ -1,45 +1,106 @@
+
+Tampermonkey® by Jan Biniok
+v5.3.3
+	
+Facebook Ad & PYMK Ultimate Blocker (v2.0)
+by YourName
+1
 // ==UserScript==
-// @name         Facebook Ad & PYMK Ultimate Blocker (v1.5)
+2
+// @name         Facebook Ad & PYMK Ultimate Blocker (v2.0)
+3
 // @namespace    http://tampermonkey.net/
-// @version      1.5
-// @description  Blocks Sponsored Posts, Suggested Posts, PYMK, and Sidebar Ads on Facebook
-// @author       obiyomida
+4
+// @version      2.0
+5
+// @description  Aggressively blocks Sponsored Posts, Suggested Posts, PYMK, and Sidebar Ads on Facebook
+6
+// @author       YourName
+7
 // @match        *://www.facebook.com/*
+8
 // @match        *://web.facebook.com/*
+9
 // @grant        none
+10
 // @run-at       document-end
+11
 // ==/UserScript==
-
+12
+​
+13
 (function() {
+14
     'use strict';
-
+15
+​
+16
     function removeElements() {
+17
         // Block Sponsored posts in News Feed
-        let sponsoredPosts = document.querySelectorAll('div:has(span:has-text("Sponsored"))');
-
+18
+        let sponsoredPosts = document.querySelectorAll('[aria-label="Sponsored"], div[data-testid="story"] [aria-label="Sponsored"], div[aria-label="Promoted"]');
+19
+​
+20
         // Block Suggested posts
-        let suggestedPosts = document.querySelectorAll('div:has(span:has-text("Suggested for you"))');
-
+21
+        let suggestedPosts = document.querySelectorAll('div:has(span:contains("Suggested for you")), div[data-testid="prefilled_tiny_feedback_like_button"], div[data-testid="feed_story_content"]:has(div[aria-label="Suggested for you"])');
+22
+​
+23
         // Block "People You May Know" (PYMK) - Feed
-        let pymkFeed = document.querySelectorAll('div:has-text("People You May Know"), div:has(span:has-text("People You May Know"))');
-
+24
+        let pymkFeed = document.querySelectorAll('div[aria-label="People You May Know"], [data-testid="facepile"] > a[href*="/friends/requests"]');
+25
+​
+26
         // Block PYMK in Sidebar & Other Sections
-        let pymkSidebar = document.querySelectorAll('[aria-label="People You May Know"], div:has(a:has-text("Add friend"))');
-
+27
+        let pymkSidebar = document.querySelectorAll('[aria-label="People You May Know"], div[data-testid="friend_requests_list"]:has(a[href*="/friends/requests"]), div[aria-label="Add friend"]');
+28
+​
+29
         // Block Ads in the Chat Sidebar (Right Panel)
-        let sidebarAds = document.querySelectorAll('div:has-text("Sponsored"):not([role="menu"])');
-
-        // Block Sidebar Ad Section more accurately
-        let sidebarAdSections = document.querySelectorAll('[aria-label="Sponsored"]');
-
+30
+        let sidebarAds = document.querySelectorAll('div:has(span:contains("Sponsored")), [aria-label="Sponsored"]');
+31
+​
+32
         // Remove all found elements
-        [...sponsoredPosts, ...suggestedPosts, ...pymkFeed, ...pymkSidebar, ...sidebarAds, ...sidebarAdSections].forEach(el => el.remove());
+33
+        [...sponsoredPosts, ...suggestedPosts, ...pymkFeed, ...pymkSidebar, ...sidebarAds].forEach(el => el.remove());
+34
     }
-
+35
+​
+36
     // Run initially
+37
     removeElements();
-
+38
+​
+39
     // Observe changes and remove new elements dynamically
-    const observer = new MutationObserver(() => removeElements());
-    observer.observe(document.body, { childList: true, subtree: true });
-})();
+40
+    const observer = new MutationObserver((mutationsList) => {
+41
+        for (let mutation of mutationsList) {
+42
+            if (mutation.type === 'childList') {
+43
+                mutation.addedNodes.forEach(node => {
+44
+                    if (node.nodeType === Node.ELEMENT_NODE) {
+45
+                        // Check each newly added node for sponsored or suggested content
+46
+                        removeElements();
+47
+                    }
+48
+                });
+49
+            }
+50
+        }
